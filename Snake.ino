1d71f31de88e419e6d5163f1ca1400b85bae7766
Erlind Sejdiu <erlindamprogrammieren@gmail.com>
2023-11-09 00:48:44 +0100

    working

--- Snake.ino
+++ Snake.ino
@@ -15,17 +15,21 @@ Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS,  TFT_DC, TFT_RST);
 #define TFT_SCLK 13   // set these to be whatever pins you like!
 #define TFT_MOSI 11   // set these to be whatever pins you like!
 //Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
-float p = 3.1415926;
+
+
+
 int SensorWert = 0;
-//int x = 10;
-//int y = 10;
+int var = 0;
+int Snakelength;
 
 struct Snake{
   int x;
   int y;
 };
-
 struct Snake S[100];
+struct Snake f; 
+
+
 void setup(void) {
   Serial.begin(9600);
   Serial.print("Hello! ST7735 TFT Test");
@@ -40,7 +44,7 @@ void setup(void) {
   tft.fillScreen(ST7735_BLACK);
   //time = millis() - time;
   Serial.println(time, DEC);
-  delay(500);
+  //delay(500);
   // large block of text
   //chart welcome = "Welcome to the greatest video game console ";
   //testdrawtext(welcome, 0);
@@ -48,16 +52,23 @@ void setup(void) {
   delay(500);
   tft.fillScreen(ST7735_WHITE);
 
-  int Snakelength = 3;
+  Snakelength = 3;
+  S[0].x = 40;
+  S[0].y = 40;
 
-  S[0].x = 0;
-  S[0].y = 0;
+  S[1].x = 30;
+  S[1].y = 40;
 
-  S[1].x = -10;
-  S[1].y = 0;
+  S[2].x = 20;
+  S[2].y = 40;
 
-  S[2].x = -20;
-  S[2].y = 0;
+  //starting Point  
+  recttangle(S[0]);
+
+  //For generating the first fruit Food 
+  f.x = 100;
+  f.y = 100;
+  recttangle(f);
 }
 
 void loop() {
@@ -65,50 +76,142 @@ void loop() {
   //delay(500);
   //tft.invertDisplay(false);
   //delay(500);
+
   SensorWert = analogRead(A0);
-  if (SensorWert > 1000)  // Wenn der Wert größer als 1010 ist, dann...
-        {
-              for(int i = 0; i < 3; i++){
-                  deleterecttangle(S[i]);
-                  S[i+1] = S[i];
-                  S[i].x += 10;
-                  recttangle(S[i]);
-              }
-              delay(50);
-        }
-    
-  if (SensorWert < 100) // Wenn der Wert kleiner als 1010 ist, dann...
-        { 
-          for(int i = 0; i < 3; i++){
-                  deleterecttangle(S[i]);
-                  S[i].x -= 10;
-                  recttangle(S[i]);
-                  //S[i+1] = S[i];
-              }
-              delay(50);
-        }
+  if (SensorWert > 1000){ var = 1;}
+  if (SensorWert < 100){ var = 2;}
   SensorWert = analogRead(A1);
-  if (SensorWert > 1000)  // Wenn der Wert größer als 1010 ist, dann...
-        {
-           for(int i = 0; i < 3; i++){
-                  deleterecttangle(S[i]);
-                  S[i].y -= 10;
-                  recttangle(S[i]);
-                  //S[i+1] = S[i];
-              }
-              delay(50);
-        }
-    
-  if (SensorWert < 100) // Wenn der Wert kleiner als 1010 ist, dann...
-        { 
-           for(int i = 0; i < 3; i++){
-                  deleterecttangle(S[i]);
-                  S[i].y += 10;
-                  recttangle(S[i]);
-                  //S[i+1] = S[i];
-              }
-              delay(50);
-        }
+  if (SensorWert > 1000){ var = 3;}
+  if (SensorWert < 100){ var = 4;} 
+  /*
+  switch (Richtung)​{
+      case 1:
+        // statements
+        S[0].x += 10;
+        break;
+      case 2:
+        // statements
+        S[0].x -= 10;
+        break;
+      case 3:
+        // statements
+        S[0].y += 10;
+        break;
+      case 4:
+        // statements
+        S[0].y -= 10;
+        break;
+      
+  }
+  */
+  switch (var) {
+  case 1:
+    //do something when var equals 1
+    //for(int i = 0; i< Snakelength; i++){
+      deleterecttangle(S[Snakelength-1]);
+    //}
+    for(int i = Snakelength-2; i > -1; i--){
+      S[i+1].y = S[i].y;
+      S[i+1].x = S[i].x;
+    }
+    //S[1] = S[0];
+    S[0].x -= 10;/*
+    for(int i = 1; i < Snakelength; i++){
+      S[i-1] = S[i];
+    }*/
+    for(int i = 0; i< Snakelength; i++){
+      recttangle(S[i]);
+    }
+    delay(1000);
+    break;
+  case 2:
+    //do something when var equals 2
+    //deleterecttangle(S[Snakelength-1]);
+    //S[Snakelength] = S[Snakelength -1];
+    //for(int i = 0; i< Snakelength; i++){
+      deleterecttangle(S[Snakelength-1]);
+    //}
+    for(int i = Snakelength-2; i > -1; i--){
+      S[i+1].y = S[i].y;
+      S[i+1].x = S[i].x;
+    }
+    //S[1] = S[0];
+    S[0].x += 10;
+    /*for(int i = Snakelength -1; i > 1; i--){
+      S[i-1] = S[i];
+    }*/
+    for(int i = 0; i< Snakelength; i++){
+      recttangle(S[i]);
+    }
+    delay(1000);
+    break;
+  case 3:
+    //do something when var equals 2
+    //for(int i = 0; i< Snakelength; i++){
+      deleterecttangle(S[Snakelength-1]);
+    //}
+    for(int i = Snakelength-2; i > -1; i--){
+      S[i+1].y = S[i].y;
+      S[i+1].x = S[i].x;
+    }
+    //S[1] = S[0];
+    S[0].y += 10;/*
+    for(int i = Snakelength -1; i > 1; i--){
+      S[i-1] = S[i];
+    }*/
+    for(int i = 0; i< Snakelength; i++){
+      recttangle(S[i]);
+    }
+    //S[Snakelength] = S[Snakelength -1];
+    //S[0].y += 10;
+    //recttangle(S[0]);
+    delay(1000);
+    break;
+  case 4:
+    //do something when var equals 2
+    //for(int i = 0; i< Snakelength; i++){
+      deleterecttangle(S[Snakelength-1]);
+    //}
+    for(int i = Snakelength-2; i > -1; i--){
+      S[i+1].y = S[i].y;
+      S[i+1].x = S[i].x;
+    }
+    //S[1] = S[0];
+    S[0].y -= 10;
+    /*for(int i = Snakelength -1; i > 1; i--){
+      S[i-1] = S[i];
+    }*/
+    for(int i = 0; i< Snakelength; i++){
+      recttangle(S[i]);
+    }
+    delay(1000);
+    break;
+  default:
+    // if nothing else matches, do the default
+    // default is optional
+    break;
+  }
+  //deleterecttangle(S[0]);
+  //recttangle(S[0]);
+  //delay(1000);
+
+  Serial.print((String)"Das ist X.1 " + S[0].x + "Das ist Y.1 " + S[0].y + "\n");
+  Serial.print((String)"Das ist X.2 " + S[1].x + "Das ist Y.2 " + S[1].y + "\n");
+  Serial.print((String)"Das ist X.3 " + S[2].x + "Das ist Y.3 " + S[2].y + "\n \n \n");
+}
+
+void generatefood(){
+    if(S[0].x == f.x && S[0].y == f.y){
+    Snakelength++;
+    deleterecttangle(f);
+    int xtemp; 
+    int ytemp;
+    xtemp = random(120); 
+    ytemp = random(160);
+    f.x = xtemp - (xtemp%10);
+    f.y = ytemp - (ytemp%10);
+    recttangle(f);
+  }
 }
 
 void testdrawtext(char *text, uint16_t color) {
@@ -117,21 +220,6 @@ void testdrawtext(char *text, uint16_t color) {
   tft.setTextWrap(true);
   tft.print(text);
 }
-/*
-void deleterecttangle(int i) {
-  tft.drawLine(x[i], y[i], x[i] + 10, y[i] , ST7735_WHITE);
-  tft.drawLine(x[i], y[i], x[i], y[i] + 10, ST7735_WHITE);
-  tft.drawLine(x[i] + 10, y[i], x[i] + 10, y[i] + 10, ST7735_WHITE);
-  tft.drawLine(x[i], y[i] + 10, x[i] + 10, y[i] + 10, ST7735_WHITE);
-}
-*//*
-void recttangle(struct Snake S[100]) {
-  int k = a[i];
-  tft.drawLine(S[i].x, S[i].y, S[i].x + 10, S[i].y , ST7735_BLACK);
-  tft.drawLine(S[i].x, S[i].y, S[i].x, S[i].y + 10, ST7735_BLACK);
-  tft.drawLine(S[i].x + 10 , S[i].y, S[i].x + 10, S[i].y +10, ST7735_BLACK);
-  tft.drawLine(S[i].x, S[i].y + 10, S[i].x + 10, S[i].y +10 , ST7735_BLACK);
-}*/
 
 void recttangle(struct Snake S) {
   tft.drawLine(S.x, S.y, S.x + 10, S.y , ST7735_BLACK);
@@ -145,196 +233,4 @@ void deleterecttangle(struct Snake S) {
   tft.drawLine(S.x, S.y, S.x, S.y + 10, ST7735_WHITE);
   tft.drawLine(S.x + 10 , S.y, S.x + 10, S.y +10, ST7735_WHITE);
   tft.drawLine(S.x, S.y + 10, S.x + 10, S.y +10 , ST7735_WHITE);
-}
-/*
-void deleterecttangleOne() {
-  tft.drawLine(x[1], y[1], x[1] + 10, y[1] , ST7735_WHITE);
-  tft.drawLine(x[1], y[1], x[1], y[1] + 10, ST7735_WHITE);
-  tft.drawLine(x[1] + 10, y[1], x[1] + 10, y[1] + 10, ST7735_WHITE);
-  tft.drawLine(x[1], y[1] + 10, x[1] + 10, y[1] + 10, ST7735_WHITE);
-}
-
-void recttangleOne() {
-  tft.drawLine(x[1], y[1], x[1] + 10, y[1] , ST7735_BLACK);
-  tft.drawLine(x[1], y[1], x[1], y[1] + 10, ST7735_BLACK);
-  tft.drawLine(x[1] + 10, y[1], x[1] + 10, y[1] + 10, ST7735_BLACK);
-  tft.drawLine(x[1], y[1] + 10, x[1] + 10, y[1] + 10, ST7735_BLACK);
-}
-*/
-/*
-void recttangle() {
-  tft.drawLine(x, y, x + 20, y , ST7735_BLACK);
-  tft.drawLine(x, y, x, y + 20, ST7735_BLACK);
-  tft.drawLine(x + 20, y, x + 20, y + 20, ST7735_BLACK);
-  tft.drawLine(x, y + 20, x + 20, y + 20, ST7735_BLACK);
-}
-*/
-
-void testlines(uint16_t color) {
-  tft.fillScreen(ST7735_BLACK);
-  for (int16_t x=0; x < tft.width(); x+=6) {
-    tft.drawLine(0, 0, x, tft.height()-1, color);
-  }
-  for (int16_t y=0; y < tft.height(); y+=6) {
-    tft.drawLine(0, 0, tft.width()-1, y, color);
-  }
-  tft.fillScreen(ST7735_BLACK);
-  for (int16_t x=0; x < tft.width(); x+=6) {
-    tft.drawLine(tft.width()-1, 0, x, tft.height()-1, color);
-  }
-  for (int16_t y=0; y < tft.height(); y+=6) {
-    tft.drawLine(tft.width()-1, 0, 0, y, color);
-  }
-  tft.fillScreen(ST7735_BLACK);
-  for (int16_t x=0; x < tft.width(); x+=6) {
-    tft.drawLine(0, tft.height()-1, x, 0, color);
-  }
-  for (int16_t y=0; y < tft.height(); y+=6) {
-    tft.drawLine(0, tft.height()-1, tft.width()-1, y, color);
-  }
-  tft.fillScreen(ST7735_BLACK);
-  for (int16_t x=0; x < tft.width(); x+=6) {
-    tft.drawLine(tft.width()-1, tft.height()-1, x, 0, color);
-  }
-  for (int16_t y=0; y < tft.height(); y+=6) {
-    tft.drawLine(tft.width()-1, tft.height()-1, 0, y, color);
-  }
-}
-/*
-void testdrawtext(char *text, uint16_t color) {
-  tft.setCursor(0, 0);
-  tft.setTextColor(color);
-  tft.setTextWrap(true);
-  tft.print(text);
-}*/
-void testfastlines(uint16_t color1, uint16_t color2) {
-  tft.fillScreen(ST7735_BLACK);
-  for (int16_t y=0; y < tft.height(); y+=5) {
-    tft.drawFastHLine(0, y, tft.width(), color1);
-  }
-  for (int16_t x=0; x < tft.width(); x+=5) {
-    tft.drawFastVLine(x, 0, tft.height(), color2);
-  }
-}
-void testdrawrects(uint16_t color) {
-  tft.fillScreen(ST7735_BLACK);
-  for (int16_t x=0; x < tft.width(); x+=6) {
-    tft.drawRect(tft.width()/2 -x/2, tft.height()/2 -x/2 , x, x, color);
-  }
-}
-void testfillrects(uint16_t color1, uint16_t color2) {
-  tft.fillScreen(ST7735_BLACK);
-  for (int16_t x=tft.width()-1; x > 6; x-=6) {
-    tft.fillRect(tft.width()/2 -x/2, tft.height()/2 -x/2 , x, x, color1);
-    tft.drawRect(tft.width()/2 -x/2, tft.height()/2 -x/2 , x, x, color2);
-  }
-}
-void testfillcircles(uint8_t radius, uint16_t color) {
-  for (int16_t x=radius; x < tft.width(); x+=radius*2) {
-    for (int16_t y=radius; y < tft.height(); y+=radius*2) {
-      tft.fillCircle(x, y, radius, color);
-    }
-  }
-}
-void testdrawcircles(uint8_t radius, uint16_t color) {
-  for (int16_t x=0; x < tft.width()+radius; x+=radius*2) {
-    for (int16_t y=0; y < tft.height()+radius; y+=radius*2) {
-      tft.drawCircle(x, y, radius, color);
-    }
-  }
-}
-void testtriangles() {
-  tft.fillScreen(ST7735_BLACK);
-  int color = 0xF800;
-  int t;
-  int w = tft.width()/2;
-  int x = tft.height()-1;
-  int y = 0;
-  int z = tft.width();
-  for(t = 0 ; t <= 15; t++) {
-    tft.drawTriangle(w, y, y, x, z, x, color);
-    x-=4;
-    y+=4;
-    z-=4;
-    color+=100;
-  }
-}
-void testroundrects() {
-  tft.fillScreen(ST7735_BLACK);
-  int color = 100;
-  int i;
-  int t;
-  for(t = 0 ; t <= 4; t+=1) {
-    int x = 0;
-    int y = 0;
-    int w = tft.width()-2;
-    int h = tft.height()-2;
-    for(i = 0 ; i <= 16; i+=1) {
-      tft.drawRoundRect(x, y, w, h, 5, color);
-      x+=2;
-      y+=3;
-      w-=4;
-      h-=6;
-      color+=1100;
-    }
-    color+=100;
-  }
-}
-void tftPrintTest() {
-  tft.setTextWrap(false);
-  tft.fillScreen(ST7735_BLACK);
-  tft.setCursor(0, 30);
-  tft.setTextColor(ST7735_RED);
-  tft.setTextSize(1);
-  tft.println("Hello World!");
-  tft.setTextColor(ST7735_YELLOW);
-  tft.setTextSize(2);
-  tft.println("Hello World!");
-  tft.setTextColor(ST7735_GREEN);
-  tft.setTextSize(3);
-  tft.println("Hello World!");
-  tft.setTextColor(ST7735_BLUE);
-  tft.setTextSize(4);
-  tft.print(1234.567);
-  delay(1500);
-  tft.setCursor(0, 0);
-  tft.fillScreen(ST7735_BLACK);
-  tft.setTextColor(ST7735_WHITE);
-  tft.setTextSize(0);
-  tft.println("Hello World!");
-  tft.setTextSize(1);
-  tft.setTextColor(ST7735_GREEN);
-  tft.print(p, 6);
-  tft.println(" Want pi?");
-  tft.println(" ");
-  tft.print(8675309, HEX); // print 8,675,309 out in HEX!
-  tft.println(" Print HEX!");
-  tft.println(" ");
-  tft.setTextColor(ST7735_WHITE);
-  tft.println("Sketch has been");
-  tft.println("running for: ");
-  tft.setTextColor(ST7735_MAGENTA);
-  tft.print(millis() / 1000);
-  tft.setTextColor(ST7735_WHITE);
-  tft.print(" seconds.");
-}
-void mediabuttons() {
-  // play
-  tft.fillScreen(ST7735_BLACK);
-  tft.fillRoundRect(25, 10, 78, 60, 8, ST7735_WHITE);
-  tft.fillTriangle(42, 20, 42, 60, 90, 40, ST7735_RED);
-  delay(500);
-  // pause
-  tft.fillRoundRect(25, 90, 78, 60, 8, ST7735_WHITE);
-  tft.fillRoundRect(39, 98, 20, 45, 5, ST7735_GREEN);
-  tft.fillRoundRect(69, 98, 20, 45, 5, ST7735_GREEN);
-  delay(500);
-  // play color
-  tft.fillTriangle(42, 20, 42, 60, 90, 40, ST7735_BLUE);
-  delay(50);
-  // pause color
-  tft.fillRoundRect(39, 98, 20, 45, 5, ST7735_RED);
-  tft.fillRoundRect(69, 98, 20, 45, 5, ST7735_RED);
-  // play color
-  tft.fillTriangle(42, 20, 42, 60, 90, 40, ST7735_GREEN);
 }
